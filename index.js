cbeccf0bd5d85f60239565c9b4ec62a267785028
Natale Toscano <kickalliance195@gmail.com>
2020-04-16 14:15:09 -0400

    First commit; basic file layout

---
+++ index.js
@@ -0,0 +1,54 @@
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
+
+let index, contact, about, fourOhFour;
+let currentData;
+
+fs.readFile('index.html', function(err, data) {
+	if(err) console.log(err);
+	index = data;
+});
+
+fs.readFile('contactMe.html', function(err, data) {
+	if(err) console.log(err);
+	contact = data;
+});
+
+fs.readFile('about.html', function(err, data) {
+	if(err) console.log(err);
+	about = data;
+});
+
+fs.readFile('404.html', function(err, data) {
+	if(err) console.log(err);
+	fourOhFour = data;
+});
+
+http.createServer(function(req, res) {
+	let q = url.parse(req.url, true);
+	console.log(q.pathname=="/about");
+	switch(q.pathname) {
+		case "/":
+		case "":
+			currentData = index;
+			break;
+		case "/about":
+		case "/about/":
+			currentData = about;
+			break;
+		case "/contact-me":
+		case "/contact-me/":
+		case "/contact":
+		case "/contact/":
+			currentData = contact;
+			break;
+		default:
+			console.log("Executing default");
+			currentData = fourOhFour;
+			break;
+	}
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.write(currentData);
+	res.end();
+}).listen(8080);
